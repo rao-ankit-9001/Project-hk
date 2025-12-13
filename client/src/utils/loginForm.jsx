81fac5454d7e7ffc5cade9ebd56ff65c3d3f535f
rao-ankit-9001 <raoankityadav9001@gmail.com>
2025-12-13 18:01:34 +0530

    update

--- client/src/utils/loginForm.jsx
+++ client/src/utils/loginForm.jsx
@@ -16,7 +16,8 @@ export default function LoginForm({ setLoading }) {
       const { latitude, longitude } = pos.coords;
 
       try {
-        const res = await fetch("http://localhost:5000/login", {
+        const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+        const res = await fetch(`${API_URL}/login`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ username, password, latitude, longitude }),
